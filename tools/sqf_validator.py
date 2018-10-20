45aa770fdabc14973eda1937a291494192b77f7f
BlenderRUS <sub7blender@gmail.com>
2018-10-20 12:45:07 +0300

    Update tools/sqf_validator.py

--- tools/sqf_validator.py
+++ tools/sqf_validator.py
@@ -110,9 +110,9 @@ def check_sqf_syntax(filepath):
                                 print("ERROR: Possible missing curly brace '}}' detected at {0} Line number: {1}".format(filepath,lineNumber))
                                 bad_count_file += 1
                             brackets_list.append('}')
-                        elif (c== '\t'):
-                            print("ERROR: Tab detected at {0} Line number: {1}".format(filepath,lineNumber))
-                            bad_count_file += 1
+                        #elif (c== '\t'):
+                            #print("ERROR: Tab detected at {0} Line number: {1}".format(filepath,lineNumber))
+                            #bad_count_file += 1
 
                         if (checkForSemicolon):
                             if (c not in [' ', '\t', '\n', '/']): # keep reading until no white space or comments
