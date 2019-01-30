3826883c146e3953c6de31f0dfb6aadfe6160d07
Kiker01 <kakycha@gmail.com>
2019-01-30 22:57:07 +0300

    бтр-40, САУ, Укрепления

--- addons/ap_btr40/config.cpp
+++ addons/ap_btr40/config.cpp
@@ -180,9 +180,9 @@ class CfgVehicles
 					initAngleY = 0;
 					minAngleY = -150;
 					maxAngleY = 150;
-					initFov = 0.7;
-					minFov = 0.25;
-					maxFov = 1.1;
+					initFov=0.69999999;
+					minFov=0.25;
+					maxFov=1.1;
 				};
 				class ViewOptics
 				{
@@ -192,9 +192,9 @@ class CfgVehicles
 					initAngleY = 0;
 					minAngleY = -360;
 					maxAngleY = 360;
-					initFov = 0.093;
-					minFov = 0.093;
-					maxFov = 0.093;
+					initFov = 0.06;
+					minFov = 0.06;
+					maxFov = 0.06;
 					weaponInfoType = "RscWeaponZeroing";
 					discreteDistance[] = {250,500,750,1000,1250,1500,1750,2000,2500};
 					discreteDistanceInitIndex = 0;
@@ -340,9 +340,9 @@ class CfgVehicles
 					initAngleY = 0;
 					minAngleY = -360;
 					maxAngleY = 360;
-					initFov = 0.4;
-					minFov = 0.4;
-					maxFov = 0.4;
+					initFov=0.69999999;
+					minFov=0.25;
+					maxFov=1.1;
 				};
 				class ViewGunner
 				{
@@ -350,11 +350,11 @@ class CfgVehicles
 					minAngleX = -25;
 					maxAngleX = 80;
 					initAngleY = 0;
-					minAngleY = -360;
-					maxAngleY = 360;
-					initFov = 0.7;
-					minFov = 0.7;
-					maxFov = 0.7;
+					minAngleY = -150;
+					maxAngleY = 150;
+					initFov=0.69999999;
+					minFov=0.25;
+					maxFov=1.1;
 				};
 				selectionFireAnim = "FRONT_zasleh";			
 				gunBeg = "muzzle_1";
@@ -455,9 +455,9 @@ class CfgVehicles
 					initAngleY = 0;
 					minAngleY = -360;
 					maxAngleY = 360;
-					initFov = 0.4;
-					minFov = 0.4;
-					maxFov = 0.4;
+					initFov=0.69999999;
+					minFov=0.25;
+					maxFov=1.1;
 				};
 				class ViewGunner
 				{
@@ -465,11 +465,11 @@ class CfgVehicles
 					minAngleX = -25;
 					maxAngleX = 80;
 					initAngleY = 0;
-					minAngleY = -360;
-					maxAngleY = 360;
-					initFov = 0.7;
-					minFov = 0.7;
-					maxFov = 0.7;
+					minAngleY = -150;
+					maxAngleY = 150;
+					initFov=0.69999999;
+					minFov=0.25;
+					maxFov=1.1;
 				};
 			};
 		};
