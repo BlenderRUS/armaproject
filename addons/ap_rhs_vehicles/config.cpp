26d80d532d2efc21ff0833c86f03bb22f50f0ea3
jrmngnd349 <jrmngnd349@gmail.com>
2019-02-16 20:44:16 +0500

    AP_RHS_VEHICLES
    - Увеличено бронирование БТР-80 и БТР-82А с 7мм до 10мм
    - Увеличено бронирование Спрута с 8мм до 10мм
    - Увеличено бронирование БМД-4М с 8мм до 12мм
    - Починено направление вращение рулей у БТР-80 и БТР-82А
    - Починен лод firegeometry у Спрута (в нём не было прокси экипажа)

--- addons/ap_rhs_vehicles/config.cpp
+++ addons/ap_rhs_vehicles/config.cpp
@@ -492,8 +492,8 @@ class CfgVehicles
 		ace_p_fire_turret = 0.7;
 		ace_p_fire_engine = 0.7;
 		ace_p_firedetonation = 0.5;
-		ace_armor_hull[] = {{10,10},{7,7},{7,7},{7,7},{7,7}};
-		ace_armor_turret[] = {{10,10},{7,7},{7,7},{7,7},{7,7}};
+		ace_armor_hull[] = {{12,12},{10,10},{10,10},{10,10},{10,10}};
+		ace_armor_turret[] = {{12,12},{10,10},{10,10},{10,10},{10,10}};
 		
 		side = 0;
 		typicalCargo[]={RU_Soldier_Crew,RU_Soldier_Crew,RU_Soldier_Crew,RU_Soldier,RU_Soldier_MG,RU_Soldier_AT};
@@ -811,8 +811,8 @@ class CfgVehicles
 		ace_p_fire_turret = 0.7;
 		ace_p_fire_engine = 0.7;
 		ace_p_firedetonation = 0.5;
-		ace_armor_hull[] = {{30,30},{8,8},{8,8},{8,8},{8,8}};
-		ace_armor_turret[] = {{30,30},{8,8},{8,8},{8,8},{8,8}};
+		ace_armor_hull[] = {{30,30},{10,10},{10,10},{10,10},{10,10}};
+		ace_armor_turret[] = {{30,30},{10,10},{10,10},{10,10},{10,10}};
 		
 		smokeLauncherGrenadeCount = 3;
 		smokeLauncherVelocity = 14;
@@ -1198,7 +1198,7 @@ class CfgVehicles
 		ace_tankfcs_digitsstyle = 1;
 		ace_tankfcs_rdystyle = 1;
 		ace_tankfcs_maxranges[] = {"250Rnd_30mmHE_2A72",3000,"250Rnd_30mmAP_2A72",3000,"34Rnd_100mm_HE_2A70",4000,"250Rnd_762x54_PKT_T90",1600};
-		ace_armor_hull[] = {{25,25},{8,8},{8,8},{8,8},{8,8}};
+		ace_armor_hull[] = {{25,25},{12,12},{12,12},{12,12},{12,12}};
 		ace_armor_turret[] = {{30,30},{16,16},{16,16},{16,16},{16,16}};
 		
 		driverAction = "Abrams_Driverout";
