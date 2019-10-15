109d47952d8cd1be0374bd84d3b49def26628d8a
Kiker01 <kakycha@gmail.com>
2019-10-15 16:48:57 +0300

    fix пустых sparty

---
+++ addons/spartillery/config.cpp
@@ -0,0 +1,7 @@
+class CfgPatches {
+	class spartillery_lev {
+		units[] = {""};
+		weapons[] = {};
+		requiredAddons[] = {"CACharacters","CAWeapons","CA_Anims_Char","CAData","CA_E","CACharacters","CACharacters2","CAWeapons","CAWeapons_E","CAAir","CAAir_e","CAAir2","CAAir_E_AH6J", "ace_c_vehicle", "acex_ru_veh_su25", "acex_ru_veh_su27", "acex_ru_veh_su30", "acex_ru_veh_su34", "tu_UH1", "tu_ah1", "acex_veh_mah6","Extended_EventHandlers", "ace_main", "ace_sys_vehicle", "ace_sys_missileguidance", "ace_sys_mfd", "ace_sys_air_fcs","tu_vehicles_sov","smd_assets","tu_missilebox_cfg"};
+	};
+};
